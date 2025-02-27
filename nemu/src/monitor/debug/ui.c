154aabb02d979b38284181361bff257c43fca33d
tracer-ics2017 <tracer@njuics.org>
2025-02-27 15:13:42 +0800

    >  compile 2211871 yyh Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  15:13:42 up 43 min,  1 user,  load average: 0.49, 0.29, 0.19 3bb95266dc9bd757f3b0965dc9bcfd2f11064d14

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,21 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args) {
+  int steps = 1;
+  if(args){
+    steps = atoi(args);
+    if(steps < 0){
+      printf("invalid steps!");  
+    }
+  }
+  cpu_exec(steps);
+  return 0;
+}
+
+
+
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,7 +61,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si", "Execute N steps", cmd_si},
   /* TODO: Add more commands */
 
 };
