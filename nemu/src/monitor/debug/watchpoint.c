6c92a7e5c4aefd9cb7283ee97163c23efab65e53
tracer-ics2017 <tracer@njuics.org>
2025-03-17 18:07:25 +0800

    >  compile 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  18:07:25 up  2:33,  1 user,  load average: 0.06, 0.13, 0.16 3ce5ecc79202267fb58efbf8f0225cb0c89355e5

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -30,7 +30,7 @@ WP* new_wp(char* e){
       free_->next->tail = free_->tail;
       free_ = free_->next;
     }
-    
+
     newWp->next = NULL;
     newWp->expr = e;
     if(head == NULL){
@@ -41,8 +41,8 @@ WP* new_wp(char* e){
       head->tail = newWp;
     }
     
-    printf("The head number is: %d\n", head->NO);
-    printf("The head string is: %s\n", head->expr);
+    // printf("The head number is: %d\n", head->NO);
+    // printf("The head string is: %s\n", head->expr);
     return newWp;
   }
   
