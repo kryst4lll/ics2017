e925d9e1d9b4babcb31d18ef4c566a3f607e4d1f
tracer-ics2017 <tracer@njuics.org>
2025-04-03 17:24:56 +0800

    >  compile 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  17:24:56 up 54 min,  1 user,  load average: 0.21, 0.17, 0.11 7b4995cb3f439d7bd3418e88695801e97f21ef45

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -1,7 +1,8 @@
 #include "cpu/exec.h"
 
 make_EHelper(add) {
-  TODO();
+  //TODO();
+  rtl_add(&t2, &id_dest->val, &id_src->val);
 
   print_asm_template2(add);
 }
@@ -9,19 +10,17 @@ make_EHelper(add) {
 make_EHelper(sub) {
   //TODO();
   rtl_sub(&t2, &id_dest->val, &id_src->val);
-  rtl_update_ZFSF(&t2,id_dest->width);
+  operand_write(id_dest,&t2);
 
+  rtl_update_ZFSF(&t2,id_dest->width);
   rtl_sltu(&t0, &id_dest->val, &id_src->val);
   rtl_set_CF(&t0);
-
-
   rtl_xor(&t0, &id_dest->val, &id_src->val);
   rtl_xor(&t1, &id_dest->val, &t2);
   rtl_and(&t0, &t0, &t1);
   rtl_msb(&t0, &t0, id_dest->width);
   rtl_set_OF(&t0);
-  operand_write(id_dest,&t2);
-
+  
   print_asm_template2(sub);
 }
 
