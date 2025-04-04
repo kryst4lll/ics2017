a55029bc8b23d915789fd1b64841fb10e8577e73
tracer-ics2017 <tracer@njuics.org>
2025-04-04 19:53:45 +0800

    >  compile 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:53:45 up  3:47,  1 user,  load average: 0.46, 0.32, 0.22 7046104ffab47acbd5bc1f61da79a154434822c4

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -84,13 +84,15 @@ make_EHelper(dec) {
 
 make_EHelper(neg) {
   //TODO();
-  rtl_eqi(&t0, &id_dest->val, 0);  
-  rtl_set_CF(&t0);                 
+  rtl_sub(&t2, &tzero, &id_dest->val);
+  rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_neq0(&t0, &id_dest->val);
+  rtl_set_CF(&t0);
 
-  rtl_sub(&t1, &tzero, &id_dest->val);  
-  operand_write(id_dest, &t1);         
+  rtl_eqi(&t0, &id_dest->val, 0x80000000);
+  rtl_set_OF(&t0);
 
-  rtl_update_ZFSF(&t1, id_dest->width);  
+  operand_write(id_dest, &t2);
 
   print_asm_template1(neg);
 }
