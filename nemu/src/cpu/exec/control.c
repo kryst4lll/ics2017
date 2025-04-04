c893a5c0764df2144bf2d727ce9d3a08b77f733c
tracer-ics2017 <tracer@njuics.org>
2025-04-04 19:49:07 +0800

    >  compile 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:49:07 up  3:43,  1 user,  load average: 0.48, 0.29, 0.19 194adff8885bf800bef4465751aa3ef9c4205cba

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -1,15 +1,13 @@
 #include "cpu/exec.h"
 
-make_EHelper(jmp)
-{
+make_EHelper(jmp) {
   // the target address is calculated at the decode stage
   decoding.is_jmp = 1;
 
   print_asm("jmp %x", decoding.jmp_eip);
 }
 
-make_EHelper(jcc)
-{
+make_EHelper(jcc) {
   // the target address is calculated at the decode stage
   uint8_t subcode = decoding.opcode & 0xf;
   rtl_setcc(&t2, subcode);
@@ -18,35 +16,30 @@ make_EHelper(jcc)
   print_asm("j%s %x", get_cc_name(subcode), decoding.jmp_eip);
 }
 
-make_EHelper(jmp_rm)
-{
+make_EHelper(jmp_rm) {
   decoding.jmp_eip = id_dest->val;
   decoding.is_jmp = 1;
 
   print_asm("jmp *%s", id_dest->str);
 }
 
-make_EHelper(call)
-{
+make_EHelper(call) {
   // the target address is calculated at the decode stage
+  //TODO();
   rtl_push(&decoding.seq_eip);
   decoding.is_jmp = 1;
   print_asm("call %x", decoding.jmp_eip);
 }
 
-make_EHelper(ret)
-{
+make_EHelper(ret) {
   //TODO();
-  rtl_pop(&t2);
-  decoding.jmp_eip = t2;
-  decoding.is_jmp = 1;
-  
-
+  rtl_pop(&t2);              
+  decoding.jmp_eip = t2;      
+  decoding.is_jmp = 1;  
   print_asm("ret");
 }
 
-make_EHelper(call_rm)
-{
+make_EHelper(call_rm) {
   //TODO();
   rtl_li(&t2, decoding.seq_eip);
   rtl_push(&t2);
