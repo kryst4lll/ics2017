538f644de3c5560f7b59ba1665aa8e84bb319c4d
tracer-ics2017 <tracer@njuics.org>
2025-04-05 20:30:04 +0800

    >  run 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:30:04 up  4:41,  1 user,  load average: 0.12, 0.20, 0.25 14c180ce4de75c503280a0c76bb633af86cecf68

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -9,7 +9,8 @@ void _ioe_init() {
 }
 
 unsigned long _uptime() {
-  return 0;
+  unsigned long current_time = inl(RTC_PORT);  // 读取当前时间戳
+  return current_time - boot_time; 
 }
 
 uint32_t* const fb = (uint32_t *)0x40000;
