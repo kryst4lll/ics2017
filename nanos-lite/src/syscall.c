c3769cd3119e679b28d4c3f2c69af1f4156d5b2c
tracer-ics2017 <tracer@njuics.org>
2025-04-18 19:06:57 +0800

    >  run 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:06:57 up  3:42,  1 user,  load average: 0.14, 0.15, 0.09 1a469c9960c62174a87038351e2984c148a7f5ed

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,20 @@
 #include "common.h"
 #include "syscall.h"
 
+int sys_none(){
+  return 1;
+}
+
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
 
   switch (a[0]) {
+    case SYS_none:
+      SYSCALL_ARG1(r) = sys_none();
+      break;
+
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
