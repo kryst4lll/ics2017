a0370291f3db8fec32ea237d18f9340934655f2c
tracer-ics2017 <tracer@njuics.org>
2025-04-23 22:09:17 +0800

    >  run 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  22:09:17 up  4:30,  1 user,  load average: 0.27, 0.21, 0.19 81583e0772b75f3c7a85881f21b99b23cf32637c

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -30,6 +30,7 @@ int fs_open(const char *pathname, int flags, int mode){
   for(int i = 0; i < NR_FILES; i++){
     if(strcmp(file_table[i].name, pathname) == 0){
       file_table[i].open_offset = 0;
+      assert(0);
       return i;
     }
   }
