3d5c579d63f9f1309d392c606c10a8cbc7270843
tracer-ics2017 <tracer@njuics.org>
2025-04-24 09:34:28 +0800

    >  run 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  09:34:28 up  1:46,  1 user,  load average: 0.15, 0.13, 0.13 12888ac7c04ea3654f3cbfcd7432bbfde051b509

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -51,17 +51,17 @@ void *_sbrk(intptr_t increment){
 
 int _read(int fd, void *buf, size_t count) {
   //_exit(SYS_read);
-  _syscall_(SYS_read, fd, (uintptr_t)buf, count);
+  return _syscall_(SYS_read, fd, (uintptr_t)buf, count);
 }
 
 int _close(int fd) {
   //_exit(SYS_close);
-  _syscall_(SYS_close, fd, 0, 0);  
+  return _syscall_(SYS_close, fd, 0, 0);  
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
   //_exit(SYS_lseek);
-  _syscall_(SYS_lseek, fd, offset, whence); 
+  return _syscall_(SYS_lseek, fd, offset, whence); 
 }
 
 // The code below is not used by Nanos-lite.
