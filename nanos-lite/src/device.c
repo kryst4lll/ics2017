e400a380f9316f8f3f5d2143f6784319250821d9
tracer-ics2017 <tracer@njuics.org>
2025-04-25 02:24:32 +0800

    >  run 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  02:24:32 up 18:37,  1 user,  load average: 0.28, 0.47, 0.35 882ad6fdd75a03af8605a5ff9d9686e7b5b60c5e

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -13,6 +13,9 @@ extern unsigned long _uptime();
 size_t events_read(void *buf, size_t len) {
   int key = _read_key();
   bool key_down = false;
+  if(key){
+    assert(0);
+  }
   if(key > 0x8000){
     key_down = true;
     key -= 0x8000;
@@ -24,8 +27,8 @@ size_t events_read(void *buf, size_t len) {
       sprintf(buf, "ku %s\n",keyname[key]);
     }
   }else{
-    // unsigned long t = _uptime();
-    // sprintf(buf, "t %d\n", t);
+    unsigned long t = _uptime();
+    sprintf(buf, "t %d\n", t);
   }
   int buf_len = strlen(buf);
   if(buf_len > len){
