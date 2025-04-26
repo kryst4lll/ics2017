45f4f1e280ad0686ac38c2a6916e4b40954c3f98
tracer-ics2017 <tracer@njuics.org>
2025-04-26 20:42:02 +0800

    >  run 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:42:02 up 11 min,  1 user,  load average: 1.20, 0.66, 0.39 49718676ad2d3bda23d21447db3bf0df97f41d97

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -78,7 +78,7 @@ _RegSet* do_syscall(_RegSet *r) {
       SYSCALL_ARG1(r) = sys_exit(a[1]);
       break;
     case SYS_write:
-      Log("1");
+      // Log("1");
       SYSCALL_ARG1(r) = sys_write(a[1], (void *)a[2], a[3]);
       break;
     case SYS_brk:
