9c2e5627cd92b2234a699a30f6b786db0c061823
tracer-ics2017 <tracer@njuics.org>
2025-05-11 15:55:23 +0800

    >  run 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  15:55:23 up  1:19,  1 user,  load average: 0.83, 0.48, 0.28 4fc9540d8303cf18a658f5d01941222bd3dc0958

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -23,12 +23,13 @@ int mm_brk(uint32_t new_brk) {
     if(new_brk > current->max_brk){
       uint32_t start_page = PGROUNDUP(current->max_brk);
       uint32_t end_page = PGROUNDDOWN(new_brk);
-      // if (start_page >= end_page) {
-      //   current->max_brk = new_brk;
-      //   return 0;
-      // }
+
       if((new_brk & 0xfff) == 0){
         end_page -= PGSIZE;
+      }
+      if (start_page > end_page) {
+        current->max_brk = new_brk;
+        return 0;
       }
 
       for(uint32_t va = start_page; va <= end_page; va += PGSIZE){
