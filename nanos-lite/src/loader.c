4dd3907651a4825c7b22ab62f9d2b901e73c6ce9
tracer-ics2017 <tracer@njuics.org>
2025-05-11 15:51:03 +0800

    >  compile 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  15:51:03 up  1:15,  1 user,  load average: 0.36, 0.26, 0.18 fdeae07f8f51cc8cc38902a0db0992d48ded2f64

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,28 +1,42 @@
 #include "common.h"
-#include "fs.h"
 #include "memory.h"
 
 #define DEFAULT_ENTRY ((void *)0x8048000)
+
+extern size_t get_ramdisk_size(void);
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
-extern size_t get_ramdisk_size();
+extern void ramdisk_write(const void *buf, off_t offset, size_t len);
+
+extern int fs_open(const char *pathname, int flags, int mode);
+extern ssize_t fs_read(int fd, void *buf, size_t len);
+extern int fs_close(int fd);
+extern size_t fs_filesz(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  int fd = fs_open(filename,0,0);
-  Log("filename = %s,fd = %d",filename,fd);
+  //TODO();
+  // size_t len = get_ramdisk_size();
+  // ramdisk_read(DEFAULT_ENTRY, 0, len);
+  int fd = fs_open(filename, 0, 0);
+  
+  size_t size = fs_filesz(fd);
+  // ssize_t read_len = fs_read(fd, DEFAULT_ENTRY, file_size);
 
-  int size = fs_filesz(fd);
-  int ppnum = size / PGSIZE;
+  // if(read_len < 0){
+  //   assert(0);
+  // }
+  int pgnum = size / PGSIZE;
   if(size % PGSIZE != 0) {
-    ppnum++;
+    pgnum++;
   }
   void *pa = NULL;
   void *va = DEFAULT_ENTRY;
-  for(int i = 0;i < ppnum;i++) {
+  for(int i = 0;i < pgnum;i++) {
     pa = new_page();
     _map(as,va,pa);
     fs_read(fd,pa,PGSIZE);
     va += PGSIZE;
-  }
+  }  
   fs_close(fd);
+
   return (uintptr_t)DEFAULT_ENTRY;
 }
