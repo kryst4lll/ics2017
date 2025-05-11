4da41c00242cdd1255dc05fa840d402d33f85623
tracer-ics2017 <tracer@njuics.org>
2025-05-11 09:53:03 +0800

    >  compile 2211871 yyh Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  09:53:03 up  1:34,  1 user,  load average: 0.36, 0.25, 0.16 db3cd401d072764657e43e1ff76bfb4d4ba75e0c

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -317,3 +317,18 @@ void operand_write(Operand *op, rtlreg_t* src) {
 make_DHelper(lidt_a) {
   decode_op_a(eip,id_dest,true);
 }
+
+make_DHelper(mov_load_cr) {
+  decode_op_rm(eip, id_dest, false, id_src, false);
+  rtl_load_cr(&id_src->val,id_src->reg);
+#ifdef DEBUG
+  snprintf(id_src->str,5,"%%cr%d",id_dest->reg);
+#endif
+}
+make_DHelper(mov_store_cr) {
+  decode_op_rm(eip, id_src, true, id_dest, false);
+
+#ifdef DEBUG
+  snprintf(id_dest->str,5,"%%cr%d",id_dest->reg);
+#endif
+}
